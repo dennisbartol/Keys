1d264b60b92e11d0051cbe056dd7deb8195a1add
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2025-12-30 21:09:36 +0100

    Update keydown event to use event.keyCode

--- keyspressed.js
+++ keyspressed.js
@@ -1,7 +1,8 @@
 window.addEventListener("keydown", (event) =>{
   document.getElementById("result").innerHTML = `The keyboard key which has been pressed is: 
-  <span> ${event.key}</span> <span>Keyboard key index: ${event.code}</span> `;
+  <span> ${event.key}</span> <span>Keyboard key index: ${event.keyCode}</span> `;
     // Edit: .keyCode being deprecated
 });
+
 
 
