06665d8d12f9405f45de28f5300a141e1db0a22f
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2026-01-01 20:18:45 +0100

    Handle space key press and update display message

--- keyspressed.js
+++ keyspressed.js
@@ -1,12 +1,15 @@
 window.addEventListener("keydown", (event) =>{
-  if(event.key === "") {
+  if(event.key === " ") {
     event.preventDefault();
   }
   
   document.getElementById("result").innerHTML = `The keyboard key which has been pressed is: 
-  <span> ${event.key}</span> <span>Keyboard key index: ${event.keyCode}</span> `;
+  <span> ${event.key === " " ? "Space" : event.key}</span> 
+  <span>Keyboard key index: ${event.keyCode}</span>
+   `;
     // Edit: .keyCode is deprecated
 });
+
 
 
 
