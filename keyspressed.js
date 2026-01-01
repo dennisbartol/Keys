6b05a901f2a7143b3d68262b4fd887b7dcd96158
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2026-01-01 20:19:02 +0100

    Update keyspressed.js

--- keyspressed.js
+++ keyspressed.js
@@ -4,11 +4,12 @@ window.addEventListener("keydown", (event) =>{
   }
   
   document.getElementById("result").innerHTML = `The keyboard key which has been pressed is: 
-  <span> ${event.key === " " ? "Space" : event.key}</span> 
+  <span>${event.key === " " ? "Space" : event.key}</span> 
   <span>Keyboard key index: ${event.keyCode}</span>
    `;
     // Edit: .keyCode is deprecated
 });
+
 
 
 
